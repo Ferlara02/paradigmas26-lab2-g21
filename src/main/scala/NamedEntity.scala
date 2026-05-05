138db28270cdc2225e3cbb9d5ccff4d28124028a
Maxi Castelle <maxicastelle@mi.unc.edu.ar>
2026-05-05 15:15:33 -0300

    Fix: correccion de los parametros en definicion de clases

--- src/main/scala/NamedEntity.scala
+++ src/main/scala/NamedEntity.scala
@@ -29,27 +29,27 @@ abstract class NamedEntity(val text: String) {
   def describe: String = s"[$entityType] $text"
 }
 
-class Person(text: String) extends NamedEntity(name) {
+class Person(text: String) extends NamedEntity(text) {
   override def entityType: String = "Person"
 }
 
-class Organization(text: String) extends NamedEntity(name){
+class Organization(text: String) extends NamedEntity(text){
   override def entityType: String = "Organization"
 }
 
-class University(text: String) extends Organization(name){
+class University(text: String) extends Organization(text){
   override def entityType: String = "University"
 }
 
-class Place(text: String) extends NamedEntity(name){
+class Place(text: String) extends NamedEntity(text){
   override def entityType: String = "Place"
 }
 
-class Technology(text: String) extends NamedEntity(name){
+class Technology(text: String) extends NamedEntity(text){
   override def entityType: String = "Technology"
 }
 
-class ProgrammingLanguage(text: String) extends Technology(name){
+class ProgrammingLanguage(text: String) extends Technology(text){
   override def entityType: String = "ProgrammingLanguage"
 }
 
